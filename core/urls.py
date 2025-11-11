da4d44ead656daa1c88cd0eec2e4033c9df231b8
Renildo15 <renildorabi@ufrn.edu.br>
2025-11-11 17:08:50 -0300

    impr: redirecionanmento

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
 from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
                                    SpectacularSwaggerView)
@@ -9,6 +10,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from auth_app.views import MyTokenObtainPairView
 
 urlpatterns = [
+    path("", lambda request: redirect("api/swagger/", permanent=False)),
     path("admin/", admin.site.urls),
     path("api/login/", MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
